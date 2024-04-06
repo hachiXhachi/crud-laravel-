6812da76a07dbd643c68e0b90ab1061e748ac00f
Jian Kyle Albaro <jiankyle69@gmail.com>
2024-04-06 14:17:16 +0800

    session login

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,18 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\middleware;
 Route::get('/', function () {
     return view('login');
 });
 Route::get('/login', function () {
     return view('login');
-});
+})->name('user.login'); 
 Route::get('/signup', [ProductController::class, 'signup'])->name('user.signup');
 Route::post('/signup', [ProductController::class, 'register'])->name('user.register');
-Route::post('/verify', [ProductController::class, 'verify'])->name('user.verify');
+Route::post('/', [ProductController::class, 'verify'])->name('user.verify');
 Route::post('/logout', [ProductController::class, 'logout'])->name('logout');
-Route::middleware('auth')->group(function () {
+Route::middleware([middleware::class])->group(function () {
     Route::get('/product', [ProductController::class, 'index'])->name('product.index');
     Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
     Route::post('/product', [ProductController::class, 'store'])->name('product.store');
